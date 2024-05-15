604eb2c84ae5ace67832b79b07ba59e0fc09e5e8
Daniross27 <163915781+Daniross27@users.noreply.github.com>
2024-05-15 16:21:20 -0600

    Add files via upload

---
+++ elevador.cpp
@@ -0,0 +1,90 @@
+#include <iostream>
+using namespace std;
+int main(){
+   int nivel;
+   cout<<"Bienvenido seleccione a que nivel desea ir del 1 al 25: ";
+   cin>>nivel;
+   switch (nivel)
+   {
+   case 1:
+   cout<<"usted esta en el nivel "<<nivel<<" puede subir al siguiete nivel";
+    break;
+      case 2:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 3:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 4:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 5:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 6:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 7:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 8:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 9:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 10:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 11:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 12:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 13:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 14:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 15:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 16:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 17:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 18:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 19:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 20:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 21:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 22:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 23:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 24:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel o subir al siguiente nivel";
+    break;
+    case 25:
+   cout<<"usted esta en el nivel "<<nivel<<" puede bajar al anterior nivel";
+    break;
+    default : cout<<"piso seleccionado invalido";
+    break;
+   }
+  
+ 
+ return 0;
+}
